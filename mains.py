e6d340fb8f2c8c9928b6c890d0397be1225d1a90
Madhura Katre <madhurakatre55@gmail.com>
2026-08-01 22:50:28 +0530

    started mains.py

--- mains.py
+++ mains.py
@@ -3,4 +3,7 @@ import chess.svg
 import streamlit as st
 from autogen import ConversableAgent, register_function
 
-
+if "gemini_api_key" not in st.session_state:
+    st.session_state.gemini_api_key = None 
+if "bored" not is  st.session_start:
+    

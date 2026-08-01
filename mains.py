f94275ee6184bb7ac9f23f24180c09d307db4693
Madhura Katre <madhurakatre55@gmail.com>
2026-08-01 23:09:54 +0530

    new lines

--- mains.py
+++ mains.py
@@ -5,5 +5,14 @@ from autogen import ConversableAgent, register_function
 
 if "gemini_api_key" not in st.session_state:
     st.session_state.gemini_api_key = None 
-if "bored" not is  st.session_start:
+if "bored" not in st.session_state:
+    st.session_state.bored = False
+if "made_move" not in st.session_state:
+    st.session_state.made_move = False
+if "board_svg" not in st.session_state:
+    st.session_state.board_svg = None
+if "move_history" not in st.session_state:
+    st.session_state.move_history = []
+if "max_truns" not in st.session_state:
+    st.session_state.max_turns = 5
     
